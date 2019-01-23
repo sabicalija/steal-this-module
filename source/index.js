64b58748508581b514bc36dd3de15611483ed2fe
Alija Sabic <sabic.alija@gmail.com>
2019-01-23 22:03:28 +0100

    Write example code

---
+++ source/index.js
@@ -0,0 +1,9 @@
+const { add } = require ("./math.js");
+const cowsay = require("cowsay");
+
+console.log(`3 + 2 = ${add(3,2)}`);
+console.log(cowsay.say({
+  text: "Steal This Module!",
+  e: "xX",
+  T: "U"
+}));
